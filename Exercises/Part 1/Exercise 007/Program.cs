51c34eefcba2399639228c478a6bf493bc94d5ab
Robin Björkholm <bjorkholmrobin@gmail.com>
2021-03-18 18:48:17 +0200

    18.3.2021

--- Exercises/Part 1/Exercise 007/Program.cs
+++ Exercises/Part 1/Exercise 007/Program.cs
@@ -7,7 +7,24 @@ namespace exercise_07
         public static void Main(string[] args)
         {
             Console.WriteLine("Give input!");
-            // Write your code here:
+            string input = Console.ReadLine();
+            int i = 0;
+
+            while(i < 3)
+            {
+            Console.WriteLine(""+ input);
+                    i++;
+            }
+            
+            
+            
+           
+            
+            
+            
+            
+            
+          
             
         }
     }
