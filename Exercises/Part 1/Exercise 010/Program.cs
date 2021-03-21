3c423f8d2ea4ac42f07b80f9be7189d56364e4b3
Robin Björkholm <bjorkholmrobin@gmail.com>
2021-03-21 15:08:22 +0200

    21.3.2021

--- Exercises/Part 1/Exercise 010/Program.cs
+++ Exercises/Part 1/Exercise 010/Program.cs
@@ -6,7 +6,20 @@ namespace exercise_10
     {
         public static void Main(string[] args)
         {
-          
+            Console.WriteLine("I will tell a story, but I need some information");
+            Console.WriteLine("please enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("please enter you profession");
+            string profession = Console.ReadLine();
+            Console.WriteLine("Here is the story!");
+            Console.WriteLine("Once upon a time there was a " +profession+ " called " +name+" on his way to work\n"+name+" often wondered what being a "+profession+" meant to them.");
+            Console.WriteLine("when you work as a "+profession+" you meet interesting people.\n"+name+" enjoys his work "+profession+" the end.");
+            
+
+              
+             
+           
+            
 
 
         }
