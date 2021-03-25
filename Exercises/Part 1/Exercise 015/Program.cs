2c05d45e35643e4f9902e7fec93693caa2bf75aa
Robin Björkholm <bjorkholmrobin@gmail.com>
2021-03-25 13:08:17 +0200

    25.3.2021

--- Exercises/Part 1/Exercise 015/Program.cs
+++ Exercises/Part 1/Exercise 015/Program.cs
@@ -7,7 +7,33 @@ namespace exercise_15
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+      Console.WriteLine("give me a string");
+      string me = Console.ReadLine();
+      Console.WriteLine(""+me);
+
+
+      Console.WriteLine("give me an integer");
+      int i = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine(""+i);
+
+
+      Console.WriteLine("give me a double");
+      string o = Console.ReadLine();
+      double doublevalue = Convert.ToDouble(o);
+      Console.WriteLine(""+o);
+
+      Console.WriteLine("give me a boolean");
+      bool truth = Convert.ToBoolean(Console.ReadLine());
+
+        if ( truth)
+        {
+        Console.WriteLine("true");
+        }
+
+        else
+        {
+        Console.WriteLine("false"); 
+        }
 
     }
   }
