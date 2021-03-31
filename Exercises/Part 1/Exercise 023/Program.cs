75e3d4ef01dfffead4bbf5591683b3e5c62276d5
Robin Björkholm <bjorkholmrobin@gmail.com>
2021-03-31 23:41:21 +0300

    31.3.2021

--- Exercises/Part 1/Exercise 023/Program.cs
+++ Exercises/Part 1/Exercise 023/Program.cs
@@ -7,7 +7,20 @@ namespace exercise_23
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+      Console.WriteLine("enter a number");
+        double i = Convert.ToDouble(Console.ReadLine());
+      Console.WriteLine("enter a 2nd number");
+        double a = Convert.ToDouble(Console.ReadLine());
+       double sum = (i+a);
+       double sub =(i-a);
+       double mult =(i*a);
+        int b = 2;
+        double avg = (sum/b);
+
+        Console.WriteLine("your sum is "+i+"+"+a+"=" +sum);
+        Console.WriteLine("your sub is "+i+"-"+a+"=" + sub);
+        Console.WriteLine("your product is "+i+"*"+a+"=" +mult);
+        Console.WriteLine("your average is "+i+"+"+a+"/2=" +avg);
 
     }
   }
