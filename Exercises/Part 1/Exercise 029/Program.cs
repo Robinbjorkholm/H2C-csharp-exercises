e1d4576ba5469810c12e14e5ee698b22588a1d9b
Robin Björkholm <bjorkholmrobin@gmail.com>
2021-04-01 00:23:47 +0300

    1.4.2021

--- Exercises/Part 1/Exercise 029/Program.cs
+++ Exercises/Part 1/Exercise 029/Program.cs
@@ -6,8 +6,23 @@ namespace exercise_29
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      Console.WriteLine("give first number");
+      int first = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("give 2nd number");
+      int second = Convert.ToInt32(Console.ReadLine());
 
+        if(first>second)
+        {
+                Console.WriteLine("the larger number is " +first );
+        } else if (first == second)
+           {
+                Console.WriteLine("the numbers are equal ");
+           }
+        else
+            {
+               Console.WriteLine("the larger number is "+second);
+            }
+        
     }
   }
 }
