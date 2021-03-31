ef3aa6a166d0ca106d1cf560341df6fabdc5055c
Robin Björkholm <bjorkholmrobin@gmail.com>
2021-03-31 23:19:37 +0300

    31.3.2021

--- Exercises/Part 1/Exercise 021/Program.cs
+++ Exercises/Part 1/Exercise 021/Program.cs
@@ -7,8 +7,14 @@ namespace exercise_21
     public static void Main(string[] args)
     {
 
-      // Write your code here:
-
+      Console.WriteLine("enter a number");
+        double i = Convert.ToDouble(Console.ReadLine());
+      Console.WriteLine("enter a 2nd number");
+        double b = Convert.ToDouble(Console.ReadLine());
+        double a = 2;
+        double sum = i+b;
+        double avg = (sum/a);
+            Console.WriteLine(avg);
     }
   }
 }
