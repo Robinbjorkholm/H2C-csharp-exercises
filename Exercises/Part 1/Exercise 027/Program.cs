92c8fd8602488b13b8fb6cab2381acd02b40902a
Robin Björkholm <bjorkholmrobin@gmail.com>
2021-04-01 00:11:45 +0300

    1.4.2021

--- Exercises/Part 1/Exercise 027/Program.cs
+++ Exercises/Part 1/Exercise 027/Program.cs
@@ -6,8 +6,18 @@ namespace exercise_27
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      Console.WriteLine("give a number");
+        int i = Convert.ToInt32(Console.ReadLine());
+        int number = 0;
 
+        if(i>= number)
+            {
+                Console.WriteLine("it is positive");
+            }
+        else
+            {
+                Console.WriteLine("it is not positive");
+            }
     }
   }
 }
