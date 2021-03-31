c6134855395a1a2555e79ff7f886d23ba36d1186
Robin Björkholm <bjorkholmrobin@gmail.com>
2021-03-31 23:05:33 +0300

    31.3.2021

--- Exercises/Part 1/Exercise 019/Program.cs
+++ Exercises/Part 1/Exercise 019/Program.cs
@@ -7,7 +7,12 @@ namespace exercise_19
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+      Console.WriteLine("enter first number");
+      int i = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("enter 2nd number");
+      int b = Convert.ToInt32(Console.ReadLine());
+      int sum = (+i+b);
+      Console.WriteLine("sum is " +i+ "+" +b+ "="   +sum);
 
     }
   }
