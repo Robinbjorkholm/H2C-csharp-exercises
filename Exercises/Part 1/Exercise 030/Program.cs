3d7ef8653f2050d8aac5a0da47aeb08d5c0b145a
Robin Björkholm <bjorkholmrobin@gmail.com>
2021-04-04 14:34:00 +0300

    4.4.2021

--- Exercises/Part 1/Exercise 030/Program.cs
+++ Exercises/Part 1/Exercise 030/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 
 namespace exercise_30
 {
@@ -6,8 +7,47 @@ namespace exercise_30
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      Console.WriteLine("give me your percent[0-100]");
+      int percent = Convert.ToInt32(Console.ReadLine());
+            if(percent <= 0 )
+            {
+             Console.WriteLine("impossible");
+            }
 
-    }
+            if(percent > 0 && percent <= 49)
+            {
+            Console.WriteLine("fail");
+            }
+
+            if(percent >= 50 && percent <= 59)
+            {
+                Console.WriteLine("your grade is 1");            
+            }
+            if(percent >= 60 && percent <= 69)
+            {
+                Console.WriteLine("your grade is 2 ");            
+            }
+            if(percent >= 70 && percent <= 79)
+            {
+                Console.WriteLine("your grade is 3");           
+            }
+
+            if(percent >= 80 && percent <= 89)
+            {
+              Console.WriteLine("your grade is 4");            
+            }
+            if(percent >=90 && percent <=100)
+            {
+            Console.WriteLine("your grade is 5");
+            }
+            if(percent > 100)
+            {
+              Console.WriteLine("outstanding!");            
+            }
+
+      
+    }       
+                
+           
   }
 }
