9107f65e6e1300ef63d8850367ff84d59d5d155e
Robin Björkholm <bjorkholmrobin@gmail.com>
2021-04-07 17:10:10 +0300

    7.4.2021

--- Exercises/Part 1/Exercise 031/Program.cs
+++ Exercises/Part 1/Exercise 031/Program.cs
@@ -6,7 +6,19 @@ namespace exercise_31
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      Console.WriteLine("enter a number");
+        int i = Convert.ToInt32(Console.ReadLine());
+        int a = 2;
+            if(i % a ==0)
+            {
+                Console.WriteLine("the number is even");       
+            }else
+            {
+                Console.WriteLine("the number is  odd");
+            }
+
+
+
 
     }
   }
