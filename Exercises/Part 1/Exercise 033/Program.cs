3a29967cdc43793fffc9f79d2ce3a062a0503379
Robin Björkholm <bjorkholmrobin@gmail.com>
2021-04-07 17:20:46 +0300

    7.4.2021

--- Exercises/Part 1/Exercise 033/Program.cs
+++ Exercises/Part 1/Exercise 033/Program.cs
@@ -6,7 +6,18 @@ namespace exercise_33
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      Console.WriteLine("enter a string");
+      string i = Console.ReadLine();
+       Console.WriteLine("enter another string");
+       string b = Console.ReadLine();
+
+        if(i == b)
+        {
+         Console.WriteLine("echo");   
+        }else
+            {
+                Console.WriteLine("Nope");
+            }
 
     }
   }
