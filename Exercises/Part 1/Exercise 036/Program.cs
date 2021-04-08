73cc2698667a039093ae5c12450cab23b612dd00
Robin Björkholm <bjorkholmrobin@gmail.com>
2021-04-08 15:37:48 +0300

    8.4.2021

--- Exercises/Part 1/Exercise 036/Program.cs
+++ Exercises/Part 1/Exercise 036/Program.cs
@@ -6,7 +6,23 @@ namespace exercise_36
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      Console.WriteLine("give a number");
+      int i = Convert.ToInt32(Console.ReadLine());
+      int b = 0;
+            if(b == i)
+            {
+                
+             // i have no idea why it prints out 0.
+                
+            }if(i < b)
+            {
+                
+                Console.WriteLine("that is negative!");
+            }else
+            {
+                Console.WriteLine(i * i);
+            
+            }
 
     }
   }
