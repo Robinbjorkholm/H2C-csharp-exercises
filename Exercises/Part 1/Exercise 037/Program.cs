417ab3b1984b1e928f65fa761855d68f7fc215d9
Robin Björkholm <bjorkholmrobin@gmail.com>
2021-04-09 16:01:56 +0300

    9.4.21

--- Exercises/Part 1/Exercise 037/Program.cs
+++ Exercises/Part 1/Exercise 037/Program.cs
@@ -8,15 +8,15 @@ namespace exercise_37
     {
       Console.WriteLine("enter numbers");
           
-      int[] numbers  = new int[10]; // how do i put a random sized array? so i just can fill it up as much as i want, so it only stops when i enter a 0
-      int a = 0;
+      int[] numbers  = new int[3]; // how do i put a random sized array? so i just can fill it up as much as i want, so it only stops when i enter a 0
+       
       
       
            
-         for ( int i = a; i < numbers.Length; i++)
+         for ( int i = 0; i < numbers.Length;i++)
          {
               numbers[i] = Convert.ToInt32(Console.ReadLine());
-                if (numbers[i]== 0)
+                if (numbers[i] < 0)
                 {
                 Console.WriteLine("total amount of numbers");
                 Console.WriteLine(numbers.Length);
--- Exercises/Part 1/Exercise 038/Program.cs
+++ Exercises/Part 1/Exercise 038/Program.cs
@@ -6,7 +6,27 @@ namespace exercise_38
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      Console.WriteLine("enter numbers");
+      int [] numbers = new int [6];
+
+      for(int i = 0;i < numbers.Length; i++)
+      {
+         Console.WriteLine("give a number");
+         numbers[i] = Convert.ToInt32(Console.ReadLine());
+
+        for(int i=0;i<numbers.length;i++)
+            {
+              if( numbers[i]<0)
+                    Console.WriteLine("here are your negative numbers");
+
+            }
+
+     
+      }
+            
+           
+
+
 
     }
   }
