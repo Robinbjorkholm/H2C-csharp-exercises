652254dd2fae3a886c19e40dcce4fb3a2d876c14
Robin Björkholm <bjorkholmrobin@gmail.com>
2021-04-10 20:11:51 +0300

    10.4.21

--- Exercises/Part 2/Exercise 044/Program.cs
+++ Exercises/Part 2/Exercise 044/Program.cs
@@ -7,8 +7,23 @@ namespace exercise_44
     public static void Main(string[] args)
     {
 
-      // Write your code here:
-
+      Console.WriteLine("enter 2 integers");
+     int i = Convert.ToInt32(Console.ReadLine());
+     int a = Convert.ToInt32(Console.ReadLine());
+    
+     if(i > a)
+     {
+         Console.WriteLine(i+" is greater than " +a);   
+     }
+            else if(i == a)
+            {   
+            Console.WriteLine(i+" is equal to " +a);
+            }
+         else if(i<a)
+            {
+                Console.WriteLine(i+ " is less than " +a);
+            }
+         
     }
   }
 }
