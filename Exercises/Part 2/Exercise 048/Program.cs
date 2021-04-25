cf19d78c28b3bfddb8fa114952f642e7f776455c
Robin Björkholm <bjorkholmrobin@gmail.com>
2021-04-25 19:29:06 +0300

    25.4.21

--- Exercises/Part 2/Exercise 048/Program.cs
+++ Exercises/Part 2/Exercise 048/Program.cs
@@ -7,8 +7,37 @@ namespace exercise_48
     public static void Main(string[] args)
     {
 
-      // Write your code here:
- 
+      Console.WriteLine("write numbers");
+      int a = -1;
+      int i = Convert.ToInt32(Console.ReadLine());
+      int sum = 0;
+      int b = 0;
+      int e = 1;
+      int even = 0;
+      int odd = 0;
+                
+       for(int c = 0;i > a;b++)
+       {
+          sum += + i;
+          i = Convert.ToInt32(Console.ReadLine());
+           if(i % 2 == 0)
+           {
+              even++;  
+           }   
+              else
+              {
+              odd++;
+              }  
+       }
+      
+        int avg = sum/b;   
+        int counter = b;
+        Console.WriteLine("thx!bye!");
+        Console.WriteLine("you entered " +counter+ " numbers");
+        Console.WriteLine("your sum is " +sum);
+        Console.WriteLine("your average is "+avg);
+        Console.WriteLine("amount of even numbers are "+even);
+        Console.WriteLine("amount of odd numbers are "+odd);
     }
   }
 }
